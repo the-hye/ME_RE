07252cdc13b614010d90817be59f249a1bdb2075
Sundou94 <71121307+Sundou94@users.noreply.github.com>
2021-09-27 23:19:38 +0900

    홈화면추가

---
+++ client/components/home/main.js
@@ -0,0 +1,10 @@
+
+//Swiper
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
